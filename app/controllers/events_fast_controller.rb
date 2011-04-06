5232c166b1c8c1988685f2cf9e50a470a894b5d9
Victor Costan <costan@gmail.com>
2011-04-06 18:11:48 -0400

    Trying to make EventsFast barebones.

--- app/controllers/events_fast_controller.rb
+++ app/controllers/events_fast_controller.rb
@@ -1,15 +1,19 @@
-class EventsFastController < ApplicationController
-  config_vars_auth :except => [:new, :create]
+class EventsFastController < ActionController::Metal
+  include ActionController::UrlFor
+  include ActionController::Routing
+  
+  include ActionController::Instrumentation
+  include ActionController::Rescue
   
   # GET /p.js
   def new
-    render :text => PwnalyticsJS::PwnalyticsJS.main_js(create_event_url)
+    self.response_body = PwnalyticsJS::PwnalyticsJS.main_js(create_event_url)
   end
 
   # GET /p.gif
   def create
     @event = Event.create_from_params params, request.user_agent,
                                       request.remote_ip
-    render :text => PwnalyticsJS::PwnalyticsJS.gif_contents
+    self.response_body = PwnalyticsJS::PwnalyticsJS.gif_contents
   end
 end
---
+++ app/helpers/events_fast_helper.rb
@@ -0,0 +1,2 @@
+module EventsFastHelper
+end
